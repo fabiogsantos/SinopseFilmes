deb3b19789ee7e2c8076ddd77669a2003be8d36c
Fabio Gonçalves <fabiogsantos@gmail.com>
2017-10-13 00:51:09 -0300

    complemento da implementação da lista de filmes

---
+++ app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/ui/GenresFragment.java
@@ -0,0 +1,171 @@
+package com.tecdam.fabiogsantos.sinopsefilmes.ui;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.tecdam.fabiogsantos.sinopsefilmes.R;
+import com.tecdam.fabiogsantos.sinopsefilmes.model.Genres;
+import com.tecdam.fabiogsantos.sinopsefilmes.viewmodel.GenresViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GenresFragment.OnGenresSelectedListener} interface
+ * to handle interaction events.
+ * Use the {@link GenresFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GenresFragment extends Fragment /*implements OnItemClickListener*/ {
+
+    private static final String ARG_GENRES = "Genres";
+
+    private GenresViewModel genresViewModel;
+
+    private Genres mGenres;
+    private ListView mListViewGenres;
+
+    private BroadcastReceiver mChangeLanguageBroadcastReceiver;
+
+    private OnGenresSelectedListener mListener;
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnGenresSelectedListener {
+        void onGenreSelected(Genres.Genre genre);
+    }
+
+    public GenresFragment() {
+        // Required empty public constructor
+    }
+
+    public static GenresFragment newInstance(Genres genres) {
+        GenresFragment fragment = new GenresFragment();
+        Bundle args = new Bundle();
+        //args.putSerializable(ARG_GENRES, genres);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mGenres = (Genres) getArguments().getSerializable(ARG_GENRES);
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mListViewGenres = getView().findViewById(R.id.lstViewGenres);
+
+        genresViewModel = ViewModelProviders.of(this).get(GenresViewModel.class);
+        genresViewModel.init(getString(R.string.apikey),getString(R.string.language));
+
+        // Carrega os dados da View por meio do WebService com LiveData
+        subscribeUi_ListGenres(genresViewModel);
+
+        // Configura os broadcasts necessários para a aplicação
+        ConfigBroadCastReceiver();
+    }
+
+    private void subscribeUi_ListGenres(GenresViewModel genresViewModel) {
+        // Update the list when the data changes
+        genresViewModel.getGenres().observe(this, new Observer<Genres>() {
+            @Override
+            public void onChanged(@Nullable Genres genres) {
+                if (genres != null) {
+                    mListViewGenres.setAdapter(new GenresAdapter(getActivity(),R.layout.view_genre, genres.genres));
+                    mListViewGenres.setOnItemClickListener(new OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                            if (mListener != null) {
+                                mListener.onGenreSelected((Genres.Genre)adapterView.getSelectedItem());
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    protected void ConfigBroadCastReceiver(){
+        if(mChangeLanguageBroadcastReceiver == null) {
+            mChangeLanguageBroadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (genresViewModel != null) {
+                        genresViewModel.refresh(getString(R.string.apikey), getString(R.string.language));
+                    }
+                }
+            };
+            IntentFilter filter = new IntentFilter(Intent.ACTION_LOCALE_CHANGED);
+            getActivity().registerReceiver(mChangeLanguageBroadcastReceiver, filter);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_genres, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //mListViewGenres = (ListView) view.findViewById(R.id.lstViewGenres);
+        //ArrayAdapter<Genres.Genre> adapterGenres = new ArrayAdapter<Genres.Genre>(getActivity(),android.R.layout.simple_list_item_1,mGenres.genres);
+        //mListViewGenres.setAdapter(adapterGenres);
+        //mListViewGenres.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnGenresSelectedListener) {
+            mListener = (OnGenresSelectedListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+/*
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (mListener != null) {
+            mListener.onGenreSelected((Genres.Genre)adapterView.getSelectedItem());
+        }
+    }
+*/
+}
--- app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/ui/MainActivity.java
+++ app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/ui/MainActivity.java
@@ -7,10 +7,6 @@ import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -19,34 +15,19 @@ import android.widget.ListView;
 import com.tecdam.fabiogsantos.sinopsefilmes.R;
 import com.tecdam.fabiogsantos.sinopsefilmes.model.Genres;
 import com.tecdam.fabiogsantos.sinopsefilmes.model.PageListMovie;
-import com.tecdam.fabiogsantos.sinopsefilmes.viewmodel.GenresViewModel;
 import com.tecdam.fabiogsantos.sinopsefilmes.viewmodel.PageListMovieViewModel;
 
-public class MainActivity extends AppCompatActivity implements LifecycleRegistryOwner, LifecycleObserver {
+public class MainActivity extends AppCompatActivity implements GenresFragment.OnGenresSelectedListener, LifecycleRegistryOwner, LifecycleObserver {
 
     LifecycleRegistry lifecycleRegistrye = new LifecycleRegistry(this);
 
-    private GenresViewModel genresViewModel;
-    private ListView listGenres;
-
     private PageListMovieViewModel pageListMovieViewModel;
     private ListView listMovies;
-
-    private BroadcastReceiver mChangeLanguageBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listGenres = findViewById(R.id.lstViewGenres);
-
-        genresViewModel = ViewModelProviders.of(this).get(GenresViewModel.class);
-        genresViewModel.init(getString(R.string.apikey),getString(R.string.language));
-
-        // Carrega os dados da View por meio do WebService com LiveData
-        subscribeUi_ListGenres(genresViewModel);
-
 
         listMovies = findViewById(R.id.lstViewMovies);
 
@@ -63,24 +44,8 @@ public class MainActivity extends AppCompatActivity implements LifecycleRegistry
         // Carrega os dados da View por meio do WebService com LiveData
         subscribeUi_ListMovies(pageListMovieViewModel);
 
-
-        // Configura os broadcasts necessários para a aplicação
-        ConfigBroadCastReceiver();
-
         // Configura o objeto que monitora a mudança de estado da activite
         getLifecycle().addObserver(this);
-    }
-
-    private void subscribeUi_ListGenres(GenresViewModel genresViewModel) {
-        // Update the list when the data changes
-        genresViewModel.getGenres().observe(this, new Observer<Genres>() {
-            @Override
-            public void onChanged(@Nullable Genres genres) {
-                if (genres != null) {
-                    listGenres.setAdapter(new GenresAdapter(getApplicationContext(),R.layout.view_genre, genres.genres));
-                }
-            }
-        });
     }
 
     private void subscribeUi_ListMovies(PageListMovieViewModel pageListMovieViewModel) {
@@ -95,23 +60,14 @@ public class MainActivity extends AppCompatActivity implements LifecycleRegistry
         });
     }
 
-    protected void ConfigBroadCastReceiver(){
-        if(mChangeLanguageBroadcastReceiver == null) {
-            mChangeLanguageBroadcastReceiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    if (genresViewModel != null) {
-                        genresViewModel.refresh(getString(R.string.apikey), getString(R.string.language));
-                    }
-                }
-            };
-            IntentFilter filter = new IntentFilter(Intent.ACTION_LOCALE_CHANGED);
-            registerReceiver(mChangeLanguageBroadcastReceiver, filter);
-        }
+    @Override
+    public void onGenreSelected(Genres.Genre genre) {
+
     }
 
     @Override
     public LifecycleRegistry getLifecycle() {
         return lifecycleRegistrye;
     }
+
 }
