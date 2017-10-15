6ee0ea0a5499dfefba5239e1eb64c53a0fb0f990
Fabio Gonçalves <fabiogsantos@gmail.com>
2017-10-14 21:30:04 -0300

    Incluido download de imagem e notificação

---
+++ app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/repository/api/NotificationDownload.java
@@ -0,0 +1,27 @@
+package com.tecdam.fabiogsantos.sinopsefilmes.repository.api;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+import com.tecdam.fabiogsantos.sinopsefilmes.R;
+
+/**
+ * Created by fabio.goncalves on 14/10/2017.
+ */
+
+public class NotificationDownload {
+
+    public void sendNotification(Context context, String title, String message) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(android.R.drawable.stat_sys_download)
+                        .setContentTitle(title)
+                        .setContentText(message);
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+}
--- app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/ui/DetailMovieActivity.java
+++ app/src/main/java/com/tecdam/fabiogsantos/sinopsefilmes/ui/DetailMovieActivity.java
@@ -1,24 +1,34 @@
 package com.tecdam.fabiogsantos.sinopsefilmes.ui;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ViewUtils;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.tecdam.fabiogsantos.sinopsefilmes.R;
 import com.tecdam.fabiogsantos.sinopsefilmes.model.Movie;
+import com.tecdam.fabiogsantos.sinopsefilmes.repository.api.NotificationDownload;
 
 public class DetailMovieActivity extends AppCompatActivity {
 
     public static final String ARG_MOVIE = "Movie";
 
     private String urlbackdrop;
+    private String urlDownload;
+    DownloadManager mDownloadManager;
+    CompleteDownload mCompleteDownload;
 
     private Movie mMovie;
     private ImageView imageViewDetailBackDropMovie;
@@ -39,6 +49,9 @@ public class DetailMovieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_movie);
 
         urlbackdrop = getString(R.string.url_themoviedb_image_medium);
+        urlDownload = getString(R.string.url_themoviedb_image_origin);
+
+        mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -83,6 +96,41 @@ public class DetailMovieActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onDownloadImage(View view) {
+        // Solicita o download da imagem
+        DownloadManager.Request mRqRequest = new DownloadManager.Request(
+                Uri.parse(urlDownload + mMovie.backdrop_path));
+        mRqRequest.setDescription(mMovie.title);
+        mDownloadManager.enqueue(mRqRequest);
+
+        // Escuta resposta da concluisão do download para enviar uma notificação para o usuario
+        if (mCompleteDownload == null) {
+            mCompleteDownload = new CompleteDownload();
+            registerReceiver(mCompleteDownload, new IntentFilter(
+                    DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mCompleteDownload);
+    }
+
+    private class CompleteDownload extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equalsIgnoreCase(
+                    DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
+
+                // Envia a notificação para usuario informado sobre a conclusão do download
+                NotificationDownload notif = new NotificationDownload();
+                notif.sendNotification(context,getString(R.string.app_label),getString(R.string.notification_download));
+            }
         }
     }
 }
